6b58b368139301a088bdc32b1327dd18e82a9103
Daniel Sidauruk <danielsidauruk11@gmail.com>
2023-01-23 14:31:11 +0700

    Add DropDownMenu

--- lib/media_plex/books/presentation/pages/book_popular_page.dart
+++ lib/media_plex/books/presentation/pages/book_popular_page.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:media_plex/core/utils/constants.dart';
+import 'package:media_plex/media_plex/books/domain/entities/book_popular.dart';
 import 'package:media_plex/media_plex/books/presentation/bloc/popular_bloc/popular_bloc.dart';
 
 class BookPopularPage extends StatefulWidget {
@@ -14,11 +15,16 @@ class BookPopularPage extends StatefulWidget {
 
 class _BookPopularPageState extends State<BookPopularPage> {
 
+  static const List<String> queryList = ['now', 'daily', 'weekly', 'monthly', 'yearly'];
+  static const List<String> queryDisplay = ['Now', 'Today', 'This Week', 'This Month', 'This Year', 'All Time'];
+
+  String dropdownValue = queryList[1];
+
   @override
   void initState() {
     super.initState();
     return BlocProvider.of<PopularBloc>(context, listen: false)
-        .add(const GetForPopular('daily'));
+        .add(GetForPopular(dropdownValue));
   }
 
   @override
@@ -42,8 +48,9 @@ class _BookPopularPageState extends State<BookPopularPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Text(
                   'Trending on : ',
@@ -55,19 +62,31 @@ class _BookPopularPageState extends State<BookPopularPage> {
                 Container(
                   alignment: Alignment.center,
                   width: 100,
+                  height: 30,
                   padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    border: Border.all(color: Colors.white)
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: Colors.white)
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('Daily'),
-                      Icon(Icons.arrow_drop_down),
-                    ],
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_drop_down, color: Colors.white,),
+                    underline: const Center(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        dropdownValue = value!;
+                        BlocProvider.of<PopularBloc>(context, listen: false)
+                            .add(GetForPopular(dropdownValue));
+                      });
+                    },
+                    items: queryList.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
-                )
+                ),
               ],
             ),
 
@@ -84,138 +103,7 @@ class _BookPopularPageState extends State<BookPopularPage> {
                       return popularLoadingAnimation();
                     } else if (state is PopularLoaded) {
                       final books = state.popular.works;
-
-                      // return GridView.builder(
-                      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      //     crossAxisCount: 2,
-                      //   ),
-                      //   itemCount: books.length,
-                      //   itemBuilder: (context, index) {
-                      //     return Container(
-                      //       height: size.height * .4,
-                      //       child: Column(
-                      //         children: [
-                      //           CachedNetworkImage(
-                      //             width: 80,
-                      //             fit: BoxFit.cover,
-                      //             imageUrl: mediumImageByCoverI('${books[index].coverI}'),
-                      //             placeholder: (context, url) =>
-                      //                 Center(
-                      //                   child: Container(
-                      //                     color: Colors.grey,
-                      //                     width: 60,
-                      //                     height: 100,
-                      //                   ),
-                      //                 ),
-                      //             errorWidget: (context, url, error) =>
-                      //                 Image.asset(
-                      //                   'assets/images/404_not_found.png',
-                      //                 ),
-                      //           ),
-                      //
-                      //           Text(
-                      //             textAlign: TextAlign.center,
-                      //             books[index].title,
-                      //             style: Theme.of(context).textTheme.subtitle2
-                      //                 ?.copyWith(fontWeight: FontWeight.w400),
-                      //             overflow: TextOverflow.ellipsis,
-                      //             maxLines: 1,
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     );
-                      //   },
-                      // );
-
-                      return ListView.builder(
-                        itemCount: books.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(8.0),
-                                margin: const EdgeInsets.symmetric(vertical: 4.0),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  border: Border.all(color: Colors.white),
-                                ),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    CachedNetworkImage(
-                                      width: 60,
-                                      fit: BoxFit.fill,
-                                      imageUrl: mediumImageByCoverI('${books[index].coverI}'),
-                                      placeholder: (context, url) => const Center(),
-                                      errorWidget: (context, url, error) =>
-                                          Image.asset(
-                                            'assets/images/404_not_found.png',
-                                          ),
-                                    ),
-
-                                    const SizedBox(width: 8.0,),
-
-                                    Expanded(
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                textAlign: TextAlign.start,
-                                                books[index].title,
-                                                style: Theme.of(context).textTheme.subtitle1
-                                                    ?.copyWith(fontWeight: FontWeight.bold),
-                                              ),
-
-                                              Row(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  const SizedBox(
-                                                    width: 25,
-                                                    child: Text('By : '),
-                                                  ),
-
-                                                  Expanded(
-                                                    child: Wrap(
-                                                      direction: Axis.horizontal,
-                                                      children: [
-                                                        ...books[index].authorName.map((item) =>
-                                                            Text('$item, ')).toList()
-                                                            .sublist(0, books[index].authorName.length-1),
-                                                        Text(books[index].authorName.last),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-
-                                          Text(
-                                            textAlign: TextAlign.start,
-                                            'First published in ${books[index].firstPublishYear}',
-                                            style: Theme.of(context).textTheme.labelMedium,
-
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-
-                                    InkWell(
-                                      onTap: (){},
-                                      child: const Icon(Icons.bookmark_border),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-
+                      return bookListView(books);
                     } else if (state is PopularError) {
                       return Text(state.message);
                     }
@@ -224,35 +112,112 @@ class _BookPopularPageState extends State<BookPopularPage> {
                 ),
               ),
             ),
-
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: [
-            //
-            //     Text('Now'),
-            //     Container(
-            //       padding: EdgeInsets.all(4.0),
-            //       decoration: BoxDecoration(
-            //         borderRadius: BorderRadius.circular(5.0),
-            //         border: Border.all(color: Colors.white)
-            //       ),
-            //       child: Text('Today', style: TextStyle(fontWeight: FontWeight.bold),),
-            //     ),
-            //     Text('This Week'),
-            //     Text('This Month'),
-            //     Text('This Year'),
-            //     Text('All Time'),
-            //   ],
-            // ),
           ],
         ),
       )
     );
   }
 
+  ListView bookListView(List<Work> books) {
+    return ListView.builder(
+      itemCount: books.length,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.symmetric(vertical: 4.0),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            border: Border.all(color: Colors.white),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CachedNetworkImage(
+                width: 60,
+                fit: BoxFit.fill,
+                imageUrl: mediumImageByCoverI('${books[index].coverI}'),
+                placeholder: (context, url) => const Center(),
+                errorWidget: (context, url, error) =>
+                    Image.asset(
+                      'assets/images/404_not_found.png',
+                    ),
+              ),
+
+              const SizedBox(width: 8.0,),
+
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.start,
+                          books[index].title,
+                          style: Theme.of(context).textTheme.subtitle1
+                              ?.copyWith(fontWeight: FontWeight.bold),
+                        ),
+
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: 25,
+                              child: Text('By : '),
+                            ),
+
+                            books[index].authorName.isNotEmpty ?
+                            Expanded(
+                              child: Wrap(
+                                direction: Axis.horizontal,
+                                children: [
+                                  ...books[index].authorName.map((item) =>
+                                      Text(
+                                        '$item, ',
+                                        style: Theme.of(context).textTheme.subtitle2
+                                            ?.copyWith(fontWeight: FontWeight.bold),
+                                      ),
+                                  ).toList()
+                                      .sublist(0, books[index].authorName.length-1),
+                                  Text(
+                                    books[index].authorName.last,
+                                    style: Theme.of(context).textTheme.subtitle2
+                                        ?.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ) :
+                            const Center(),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    Text(
+                      textAlign: TextAlign.start,
+                      'First published in ${books[index].firstPublishYear}',
+                      style: Theme.of(context).textTheme.labelMedium,
+
+                    ),
+                  ],
+                ),
+              ),
+
+              InkWell(
+                onTap: (){},
+                child: const Icon(Icons.bookmark_border),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   Container popularLoadingAnimation() {
     return Container(
-      padding: const EdgeInsets.all(8.0),
       margin: const EdgeInsets.symmetric(vertical: 4.0),
       child: Column(
         children: [
